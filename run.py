1ad790907387bcb4d76f46807da856239f5670f8
SamBailey6194 <samabailey6194@gmail.com>
2025-03-27 11:01:20 +0000

    Add random module colorama dependency and google api dependency to enable saved games and leaderboard generation

--- run.py
+++ run.py
@@ -1,1 +1,16 @@
+# Imported dependencies and modules
+from random import randint
+import colorama
+import gspread
+from google.oauth2.service_account import Credentials
 
+# Giving python access to google sheet
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('ultimate_battleships')
