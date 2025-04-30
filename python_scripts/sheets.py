d339fd9dfe49ab99d5775708063fa1e9602a653f
SamBailey6194 <samabailey6194@gmail.com>
2025-04-30 10:49:26 +0100

    Update requirements.txt and sheets.py to handle finding of the creds config var on heroku properly

--- python_scripts/sheets.py
+++ python_scripts/sheets.py
@@ -2,6 +2,8 @@
 
 # Imported dependencies and modules
 import gspread
+import json
+import os
 from google.oauth2.service_account import Credentials
 
 # Giving python access to google sheet
@@ -10,7 +12,13 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-CREDS = Credentials.from_service_account_file('creds.json')
+
+# Load credentials from environment variable
+creds_json = os.environ.get('CREDS')
+if not creds_json:
+    raise ValueError("Missing CREDS environment variable")
+
+CREDS = Credentials.from_service_account_file(json.loads(creds_json))
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('ultimate_battleships')
